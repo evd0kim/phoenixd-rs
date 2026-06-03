d3ca44c41cad3491afecbe2076633211a77ee219
Ilya Evdokimov <evdokimov.ilya@gmail.com>
2026-06-03 11:57:23 +0200

    outgoingbyhash fix

--- src/pay_ln.rs
+++ src/pay_ln.rs
@@ -187,7 +187,7 @@ impl Phoenixd {
 
         let url = self
             .api_url
-            .join(&format!("payments/outgoing/{}", payment_hash))
+            .join(&format!("payments/outgoingbyhash/{}", payment_hash))
             .map_err(|_| Error::InvalidUrl)?;
 
         let res = match self.make_get(url).await {
